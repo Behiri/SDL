2676e7bb29cad94ce1839721961e2065b4dc8595
behi iri <Behiri@users.noreply.github.com>
2015-02-08 10:01:11 +0000

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,104 @@
+#include <SDL.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Game
+{
+public:
+	Game();
+	void Init(const char* title, int x, int y, int width, int hight, int flag);
+	void render(int, int, int);
+	void clear();
+	void Image();
+	SDL_Surface* m_pImage;
+	SDL_Surface* gScreenSurface;
+	void handleEvents();
+	bool Running();
+private:
+	SDL_Window* m_pWindow;
+	SDL_Renderer* m_pRender;
+	bool m_bRunning;
+	SDL_Event* m_Event;
+
+};
+
+
+void Game::Init(const char* title = "behiri", int x = 100, int y = 100, int width = 640, int hight = 480, int flag = 0)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		m_pWindow = SDL_CreateWindow(title, x, y, width, hight, flag);
+		cout << "window Created" << endl;
+		if (m_pWindow != 0)
+		{
+			m_pRender = SDL_CreateRenderer(m_pWindow, -1, 0);
+		}
+	}
+	else
+	{
+		cout << "error: failed to  Initializing SDL" << endl;
+	}
+}
+
+void Game::render(int R, int G, int B)
+{
+	if (m_pWindow != 0)
+	{
+		SDL_SetRenderDrawColor(m_pRender,R = 0, G = 0, B = 0, 1);
+		SDL_RenderClear(m_pRender);
+		cout << "render Created" << endl;
+		SDL_RenderPresent(m_pRender);
+	}
+	else
+	{
+		cout << "error: failed to create renderer" << endl;
+	}
+}
+
+Game::Game()
+{
+	m_pWindow = 0;
+	m_pRender = 0;
+	m_pImage = 0;
+	m_bRunning = true;
+};
+
+void Game::clear()
+{
+	SDL_Quit();
+}
+
+void Game::Image()
+{
+	gScreenSurface = SDL_GetWindowSurface( m_pWindow );
+	m_pImage = SDL_LoadBMP( "Behiri.bmp" );
+	SDL_BlitSurface( m_pImage, NULL, gScreenSurface, NULL );
+	SDL_UpdateWindowSurface( m_pWindow );
+	SDL_Delay(2000);
+}
+
+void Game::handleEvents()
+{
+	SDL_Event Event;
+	if(SDL_PollEvent(&Event))
+	{
+		switch (Event.type)
+		{
+		case SDL_QUIT:
+			{
+				m_bRunning = false;
+				break;
+			}
+		default:
+			break;
+		}
+	}
+
+}
+
+bool Game::Running()
+{
+	return m_bRunning;
+}
